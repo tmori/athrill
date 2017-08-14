030245fd8b788d298a4f4fdd6e84394b9b049a98
tmori <tmori@esm.co.jp>
2017-08-14 16:00:34 +0900

    テスト項目追加した．

---
+++ sample/os/asp3/OBJ/athrill_test/asp3_test_cychdr_1.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+. ./asp3_test_common.sh
+
+activate_athrill_quick
+
+#start test
+
+echo "## start test"
+
+athrill_remote c
+
+wait_athrill 1
+
+#wait TASK1
+athrill_remote "S 0 c"
+athrill_remote c
+
+#wait 60sec
+echo "sleep 60sec"
+wait_athrill 60
+
+echo "## end test"
+deactivate_athrill
