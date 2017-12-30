be441630633db93425d96c88f4a7a512dcdec1cc
tmori <tmori@esm.co.jp>
2017-12-31 08:07:28 +0900

    デバイス管理ファイルを追加

---
+++ trunk/src/config/target/v850e2m/mpu_config.c
@@ -0,0 +1,114 @@
+#include "mpu.h"
+#include "cpu_config.h"
+
+#define MPU_ADDRESS_REGION_MASK_PH		0x03FFFFFF
+
+#define MPU_ADDRESS_REGION_SIZE_INX_ROM		(1024U * 512U)
+#define MPU_ADDRESS_REGION_SIZE_INX_RAM		(1024U * 10240U)
+#define MPU_ADDRESS_REGION_SIZE_INX_INTC	(0xFFFFF1FB - 0xFFFFF100)
+#define MPU_ADDRESS_REGION_SIZE_INX_SERIAL	(0xFFFFFA78 - 0xFFFFFA00)
+#define MPU_ADDRESS_REGION_SIZE_INX_CPU		(32U * 4U)
+#define MPU_ADDRESS_REGION_SIZE_INX_PH0		(1024U * 4U)
+#define MPU_ADDRESS_REGION_SIZE_INX_PH1		(1024U * 12U)
+
+static uint8 memory_data_ROM[MPU_ADDRESS_REGION_SIZE_INX_ROM];
+static uint8 memory_data_RAM[MPU_ADDRESS_REGION_SIZE_INX_RAM];
+static uint8 memory_data_INTC[MPU_ADDRESS_REGION_SIZE_INX_INTC * CPU_CONFIG_CORE_NUM];
+static uint8 memory_data_SERIAL[MPU_ADDRESS_REGION_SIZE_INX_SERIAL];
+static uint8 memory_data_PH0[MPU_ADDRESS_REGION_SIZE_INX_PH0];
+static uint8 memory_data_PH1[MPU_ADDRESS_REGION_SIZE_INX_PH1];
+
+extern MpuAddressRegionOperationType	serial_memory_operation;
+extern MpuAddressRegionOperationType	intc_memory_operation;
+extern MpuAddressRegionOperationType	cpu_register_operation;
+
+MpuAddressMapType mpu_address_map = {
+		.map = {
+				/*
+				 * INDEX 0:ROM
+				 */
+				{
+						.type		= GLOBAL_MEMORY,
+						.permission	= MPU_ADDRESS_REGION_PERM_ALL,
+						.start		= 0x00000000,
+						.size		= MPU_ADDRESS_REGION_SIZE_INX_ROM,
+						.mask		= MPU_ADDRESS_REGION_MASK_ALL,
+						.data		= memory_data_ROM,
+						.ops		= &default_memory_operation
+				},
+				/*
+				 * INDEX 1:RAM
+				 */
+				{
+						.type		= GLOBAL_MEMORY,
+						.permission	= MPU_ADDRESS_REGION_PERM_ALL,
+						.start		= 0x05FF0000,
+						.size		= MPU_ADDRESS_REGION_SIZE_INX_RAM,
+						.mask		= MPU_ADDRESS_REGION_MASK_ALL,
+						.data		= memory_data_RAM,
+						.ops		= &default_memory_operation
+				},
+				/*
+				 * INDEX 2:DEVICE(割込みコントローラ)
+				 */
+				{
+						.type		= DEVICE,
+						.permission	= MPU_ADDRESS_REGION_PERM_ALL,
+						.start		= 0x03FFF100,
+						.size		= MPU_ADDRESS_REGION_SIZE_INX_INTC,
+						.mask		= MPU_ADDRESS_REGION_MASK_PH,
+						.data		= memory_data_INTC,
+						.ops		= &intc_memory_operation
+				},
+				/*
+				 * SERIAL
+				 */
+				{
+						.type		= DEVICE,
+						.permission	= MPU_ADDRESS_REGION_PERM_ALL,
+						.start		= 0x03FFFA00,
+						.size		= MPU_ADDRESS_REGION_SIZE_INX_SERIAL,
+						.mask		= MPU_ADDRESS_REGION_MASK_PH,
+						.data		= memory_data_SERIAL,
+						.ops		= &serial_memory_operation
+				},
+
+				/*
+				 * INDEX :CPUレジスタ(デバッグ用)
+				 */
+				{
+						.type		= DEVICE,
+						.permission	= MPU_ADDRESS_REGION_PERM_ALL,
+						.start		= CPU_CONFIG_DEBUG_REGISTER_ADDR,
+						.size		= MPU_ADDRESS_REGION_SIZE_INX_CPU,
+						.mask		= MPU_ADDRESS_REGION_MASK_ALL,
+						.data		= memory_data_RAM,
+						.ops		= &default_memory_operation
+				},
+
+				/*
+				 * INDEX :DEVICE(その他内蔵周辺I/O領域)
+				 */
+				{
+						.type		= DEVICE,
+						.permission	= MPU_ADDRESS_REGION_PERM_ALL,
+						.start		= 0x03FFF000,
+						.size		= MPU_ADDRESS_REGION_SIZE_INX_PH0,
+						.mask		= MPU_ADDRESS_REGION_MASK_PH,
+						.data		= memory_data_PH0,
+						.ops		= &default_memory_operation
+				},
+				/*
+				 * INDEX :DEVICE(プログラマブル周辺I/O領域)
+				 */
+				{
+						.type		= DEVICE,
+						.permission	= MPU_ADDRESS_REGION_PERM_ALL,
+						.start		= 0x03FEC000,
+						.size		= MPU_ADDRESS_REGION_SIZE_INX_PH1,
+						.mask		= MPU_ADDRESS_REGION_MASK_PH,
+						.data		= memory_data_PH1,
+						.ops		= &default_memory_operation
+				},
+		}
+};
---
+++ trunk/src/device/target/v850e2m/device.c
@@ -0,0 +1,78 @@
+#include "device.h"
+#include "cpuemu_ops.h"
+#include <stdio.h>
+
+static void device_init_clock(MpuAddressRegionType *region)
+{
+	/*
+	 * OSTC
+	 */
+	(void)device_io_write8(region, 0xFFFFF6C2, 0x01);
+
+	/*
+	 * ロック・レジスタ（ LOCKR）
+	 */
+	(void)device_io_write8(region, 0xFFFFF824, 0x00);
+
+	return;
+}
+
+
+void device_init(CpuType *cpu, DeviceClockType *dev_clock)
+{
+	dev_clock->clock = 0;
+	dev_clock->intclock = 0;
+	dev_clock->min_intr_interval = DEVICE_CLOCK_MAX_INTERVAL;
+	dev_clock->can_skip_clock = FALSE;
+
+	device_init_clock(&mpu_address_map.map[MPU_ADDRESS_REGION_INX_PH0]);
+	device_init_intc(cpu, &mpu_address_map.map[MPU_ADDRESS_REGION_INX_INTC]);
+
+	return;
+}
+
+void device_supply_clock(DeviceClockType *dev_clock)
+{
+	dev_clock->min_intr_interval = DEVICE_CLOCK_MAX_INTERVAL;
+	dev_clock->can_skip_clock = TRUE;
+
+	device_supply_clock_intc(dev_clock);
+	return;
+}
+
+
+int device_io_write8(MpuAddressRegionType *region,  uint32 addr, uint8 data)
+{
+	return region->ops->put_data8(region, CPU_CONFIG_CORE_ID_0, (addr & region->mask), data);
+}
+int device_io_write16(MpuAddressRegionType *region, uint32 addr, uint16 data)
+{
+	return region->ops->put_data16(region, CPU_CONFIG_CORE_ID_0, (addr & region->mask), data);
+}
+
+int device_io_write32(MpuAddressRegionType *region, uint32 addr, uint32 data)
+{
+	return region->ops->put_data32(region, CPU_CONFIG_CORE_ID_0, (addr & region->mask), data);
+}
+
+int device_io_read8(MpuAddressRegionType *region, uint32 addr, uint8 *data)
+{
+	return region->ops->get_data8(region, CPU_CONFIG_CORE_ID_0, (addr & region->mask), data);
+}
+
+int device_io_read16(MpuAddressRegionType *region, uint32 addr, uint16 *data)
+{
+	return region->ops->get_data16(region, CPU_CONFIG_CORE_ID_0, (addr & region->mask), data);
+}
+
+int device_io_read32(MpuAddressRegionType *region, uint32 addr, uint32 *data)
+{
+	return region->ops->get_data32(region, CPU_CONFIG_CORE_ID_0, (addr & region->mask), data);
+}
+
+void device_raise_int(uint16 intno)
+{
+	intc_raise_intr(intno);
+}
+
+
