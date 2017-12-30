87cf2bbc4f00c5f3c17e375f6a489f4f046cee31
tmori <tmori@esm.co.jp>
2017-12-30 09:05:45 +0900

    v850e2mのヘッダを追加

---
+++ trunk/src/debugger/interaction/inc/target/v850e2m/dbg_interaction_target_ids.h
@@ -0,0 +1,10 @@
+#ifndef _DBG_INTERACTION_TARGET_IDS_H_
+#define _DBG_INTERACTION_TARGET_IDS_H_
+
+#include "dbg_interaction_std_ids.h"
+
+typedef enum {
+	DBG_CMD_TARGET_ID_XXX = DBG_CMD_STD_ID_TARGET,
+} DbgCmdTargetIdType;
+
+#endif /* _DBG_INTERACTION_TARGET_IDS_H_ */
---
+++ trunk/src/device/target/v850e2m/device.h
@@ -0,0 +1,47 @@
+﻿#ifndef _DEVICE_H_
+#define _DEVICE_H_
+
+#include "std_types.h"
+#include "cpu.h"
+#include "mpu_config.h"
+#include "mpu.h"
+#include "std_device_ops.h"
+
+/*
+ * デバイス初期化関数
+ */
+extern void device_init_intc(CpuType *cpu, MpuAddressRegionType *region);
+extern void device_init_timer(MpuAddressRegionType *region);
+extern void device_init_serial(MpuAddressRegionType *region);
+extern void device_init_can(MpuAddressRegionType *region);
+
+/*
+ * デバイスクロック供給
+ */
+extern void device_supply_clock_intc(DeviceClockType *dev_clock);
+extern void device_supply_clock_timer(DeviceClockType *dev_clock);
+extern void device_supply_clock_serial(DeviceClockType *dev_clock);
+
+/*
+ * 割込み関数
+ */
+extern void device_raise_int(uint16 intno);
+
+/*
+ * メモリアクセス関数
+ */
+extern int device_io_write8(MpuAddressRegionType *region, uint32 addr, uint8 data);
+extern int device_io_write16(MpuAddressRegionType *region, uint32 addr, uint16 data);
+extern int device_io_write32(MpuAddressRegionType *region, uint32 addr, uint32 data);
+
+extern int device_io_read8(MpuAddressRegionType *region, uint32 addr, uint8 *data);
+extern int device_io_read16(MpuAddressRegionType *region, uint32 addr, uint16 *data);
+extern int device_io_read32(MpuAddressRegionType *region, uint32 addr, uint32 *data);
+
+/*
+ * 割込みコントローラAPI
+ */
+extern void intc_clr_nmi(TargetCoreType *cpu);
+extern void intc_clr_currlvl_ispr(TargetCoreType *cpu);
+
+#endif /* _DEVICE_H_ */
