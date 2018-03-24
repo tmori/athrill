b8fd37da1385b319502dd309ef797e8d21fac6ab
tmori <tmori@esm.co.jp>
2018-03-25 08:03:51 +0900

    auto select editor

--- bin/linux/geany.sh
+++ bin/linux/geany.sh
@@ -2,7 +2,21 @@
 
 SAKURA_FILE="./arg_sakura.txt"
 
-#geany -r `cat ${SAKURA_FILE}` &
-FILE=$(cat ./arg_sakura.txt | awk '{print $3}')
-LINE=$(cat ./arg_sakura.txt | awk '{print $2}')
-code -r  -g ${FILE}:${LINE}
+which code > /dev/null
+if [ $? -eq 0 ]
+then
+    FILE=$(cat ./arg_sakura.txt | awk '{print $3}')
+    LINE=$(cat ./arg_sakura.txt | awk '{print $2}')
+    code -r  -g ${FILE}:${LINE}
+else
+    which geany > /dev/null
+    if [ $? -eq 0 ]
+    then
+        geany -r `cat ${SAKURA_FILE}` &
+    else
+        echo "ERROR not found editor: please install vscode or geany!"
+        exit 1
+    fi
+fi
+
+exit 0
