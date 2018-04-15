47e44937318d285cd33c70aa2145bddf867e5789
tmori <tmori@esm.co.jp>
2018-04-15 18:25:15 +0900

    add startup init

---
+++ sample/barmetal/step2/main.c
@@ -0,0 +1,40 @@
+#include "test_serial.h"
+#include "test_reg.h"
+#include "section.h"
+#include <string.h>
+
+unsigned char stack_data[STACK_SIZE] __attribute__ ((section("bss_noclr")));
+
+static int global_value;
+static int *global_value_pointer = &global_value;
+
+int main(void)
+{
+	test_print("Hello World!!\n");
+
+	*global_value_pointer = 10;
+	while (1) {
+		;
+	}
+}
+
+void bss_clear(void)
+{
+	unsigned char *p = &_bss_start;
+	unsigned char *e = &_bss_end;
+	for (;p < e; p++) {
+		*p = 0;
+	}
+	return;
+}
+
+void data_init(void)
+{
+	unsigned char *p_rom = &_idata_start;
+	unsigned char *e_rom = &_idata_end;
+	unsigned char *p_ram = &_data_start;
+	
+	for (;p_rom < e_rom; p_ram++, p_rom++) {
+		*p_ram = *p_rom;
+	}
+}
---
+++ sample/barmetal/step2/test_serial.h
@@ -0,0 +1,20 @@
+#ifndef _TEST_SERIAL_H_
+#define _TEST_SERIAL_H_
+
+#define SERIAL_OUT_ADDR		((volatile unsigned char*)0xFFFFFA07)
+
+static inline void test_print(const char *str)
+{
+	int i;
+	for (i = 0; str[i] != '\0'; i++) {
+		*(SERIAL_OUT_ADDR) = str[i];
+	}
+	*(SERIAL_OUT_ADDR) = '\n';
+}
+
+static inline void test_print_one(const char *ch)
+{
+	*(SERIAL_OUT_ADDR) = *ch;
+}
+
+#endif /* _TEST_SERIAL_H_ */
