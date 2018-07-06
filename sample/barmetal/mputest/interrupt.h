8cd96dcd2680556c4a8878f7be5b0611da4c0c19
tmori <tmori@esm.co.jp>
2018-07-06 17:05:29 +0900

    mpu test is added

---
+++ sample/barmetal/mputest/interrupt.h
@@ -0,0 +1,23 @@
+#ifndef _INTERRUPT_H_
+#define _INTERRUPT_H_
+
+typedef unsigned char uint8;
+typedef unsigned int uint32;
+typedef unsigned char bool;
+#define TRUE 1
+#define FALSE 0
+
+/*
+ *	割込み制御レジスタの番地を算出するためのマクロ
+ *
+ *	割込み制御レジスタは割込み番号順に並んでいるため，
+ *	ベースアドレスからのオフセットでアドレスを求めることができる．
+ */
+
+#define INTREG_BASE				(0xFFFFF110)
+#define INTREG_ADDRESS(intno)	(INTREG_BASE + ((intno) * 2U))
+
+extern void x_enable_int(int intno);
+extern void x_clear_int(int intno);
+
+#endif /* _INTERRUPT_H_ */
---
+++ sample/barmetal/mputest/main.c
@@ -0,0 +1,56 @@
+#include "test_serial.h"
+#include "test_reg.h"
+#include "section.h"
+#include "interrupt.h"
+#include <string.h>
+#include "timer.h"
+
+unsigned char stack_data[STACK_SIZE] __attribute__ ((section(".bss_noclr")));
+
+unsigned char mmap_data[1024] __attribute__ ((section(".mmap_section")));
+
+static int gl_variable;
+
+static unsigned int athrill_device_func_call __attribute__ ((section(".athrill_device_section")));
+
+
+int main(void)
+{
+	char *a = "10";
+
+	int x = 1U << 19;
+
+	mmap_data[0] = x;
+
+	test_print_one(a);
+	timer_init();
+
+	printf("Hello World!!\n");
+
+
+	while (1) {
+		;
+	}
+}
+
+void bss_clear(void)
+{
+	unsigned char *p = &_bss_start;
+	unsigned char *e = &_bss_end;
+	for (;p < e; p++) {
+		*p = 0;
+	}
+	return;
+}
+
+void data_init(void)
+{
+	unsigned char *p_rom = &_idata_start;
+	unsigned char *e_rom = &_idata_end;
+	unsigned char *p_ram = &_data_start;
+
+	for (;p_rom < e_rom; p_ram++, p_rom++) {
+		*p_ram = *p_rom;
+	}
+}
+
