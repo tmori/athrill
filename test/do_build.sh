25ce1f753979adff6dccd58de48975b1858717e8
tmori <tmori@esm.co.jp>
2018-07-12 20:28:41 +0900

    add build script

---
+++ test/do_build.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+. ./test/env.sh
+
+cd trunk/src/build/target/linux_v850e2m
+make clean;make
+
+if [ -f ${ATHRILL_HOME}/bin/linux/athrill2 ]
+then
+	exit 0
+else
+	exit 1
+fi
---
+++ test/env.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+export ATHRILL_HOME=${HOME}/build/tmori/athrill
+export PATH=${PATH}:${ATHRILL_HOME}/bin/linux
+
