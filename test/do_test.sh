3f49633a044138d5d82fa358e9719af94d9ae7db
tmori <tmori@esm.co.jp>
2018-07-22 05:23:33 +0900

    add api and sil test

--- test/do_test.sh
+++ test/do_test.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-. ./test/env.sh
-
 echo "#START TEST ASP TARGET TIMER"
 sh test/scripts/do_test_asp_target_timer.sh
 if [ $? -ne 0 ]
@@ -18,6 +16,20 @@ fi
 
 echo "#START TEST ASP TARGET EXCEPTION"
 sh test/scripts/do_test_asp_target_exception.sh
+if [ $? -ne 0 ]
+then
+    exit 1
+fi
+
+echo "#START TEST ASP SIL"
+sh test/scripts/do_test_asp_sil.sh
+if [ $? -ne 0 ]
+then
+    exit 1
+fi
+
+echo "#START TEST ASP API"
+sh test/scripts/do_test_asp_api.sh
 if [ $? -ne 0 ]
 then
     exit 1
--- test/env.sh
+++ test/env.sh
@@ -5,5 +5,27 @@ export PATH=${PATH}:${ATHRILL_HOME}/bin/linux
 export CONFIG_MEMORY=${HOME}/build/tmori/athrill/test/config/memory.txt
 export CONFIG_DEBUG=${HOME}/build/tmori/athrill/test/config/device_config.txt
 export TEST_LOG=${HOME}/build/tmori/athrill/test/scripts/log
-export EXPECT_LOG=${HOME}/build/tmori/athrill/test/scripts/expect_log
 
+CMD_NAME=
+function util_set_cmdname() {
+    CMD_NAME=$( echo ${1} | awk -F\. '{print $1}' | awk -F\/ '{print $NF}' )
+    rm -f ${TEST_LOG}/${CMD_NAME}.log
+}
+
+TEST_TIMEOUT=
+TEST_BIN=
+function util_do_test() {
+    athrill2 -c1 -m ${CONFIG_MEMORY} -d ${CONFIG_DEBUG} ${TEST_BIN} -t ${TEST_TIMEOUT} | tee -a ${TEST_LOG}/${CMD_NAME}.log
+}
+
+function util_do_log_check() {
+    egrep "Assert|failed|error" ${TEST_LOG}/${CMD_NAME}.log 
+    if [ $? -ne 0 ]
+    then
+        echo "PASSED: $0"
+        exit 0
+    else
+        echo "FAILED: $0"
+        exit 1
+    fi
+}
