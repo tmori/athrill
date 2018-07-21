32280f6b643e917518e504750d21cec143bf25be
tmori <tmori@esm.co.jp>
2018-07-21 11:22:43 +0900

    env is modified

--- test/do_test.sh
+++ test/do_test.sh
@@ -1,3 +1,26 @@
 #!/bin/sh
 
+. ./test/env.sh
 
+echo "#START TEST ASP TARGET TIMER"
+sh test/scripts/do_test_asp_target_timer.sh
+if [ $? -ne 0 ]
+then
+    exit 1
+fi
+
+echo "#START TEST ASP TARGET INTERRUPT"
+sh test/scripts/do_test_asp_target_interrupt.sh
+if [ $? -ne 0 ]
+then
+    exit 1
+fi
+
+echo "#START TEST ASP TARGET EXCEPTION"
+sh test/scripts/do_test_asp_target_exception.sh
+if [ $? -ne 0 ]
+then
+    exit 1
+fi
+
+exit 0
--- test/env.sh
+++ test/env.sh
@@ -2,4 +2,8 @@
 
 export ATHRILL_HOME=${HOME}/build/tmori/athrill
 export PATH=${PATH}:${ATHRILL_HOME}/bin/linux
+export CONFIG_MEMORY=${HOME}/build/tmori/athrill/test/config/memory.txt
+export CONFIG_DEBUG=${HOME}/build/tmori/athrill/test/config/device_config.txt
+export TEST_LOG=${HOME}/build/tmori/athrill/test/scripts/log
+export EXPECT_LOG=${HOME}/build/tmori/athrill/test/scripts/expect_log
 
