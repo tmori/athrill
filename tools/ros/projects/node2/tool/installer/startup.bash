69da1b23f233e3d0fcf936158a319efb1588afc2
tmori@esm.co.jp <tmori@esm.co.jp>
2018-10-20 16:44:05 +0900

    bug fixed

--- tools/ros/projects/node2/tool/installer/startup.bash
+++ tools/ros/projects/node2/tool/installer/startup.bash
@@ -1,1 +1,10 @@
-../launcher/startup.bash
+#!/bin/bash
+
+source ../env/env.sh
+
+CURDIR=`pwd`
+
+cd ${ROSWORK_DIR}
+source ./devel/setup.bash
+
+roslaunch ./launch/hakoniwa.launch
--- tools/ros/projects/node2/tool/launcher/startup.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-source ../env/env.sh
-
-CURDIR=`pwd`
-
-cd ${ROSWORK_DIR}
-
-roslaunch ./launch/hakoniwa.launch
