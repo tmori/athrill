d42c2c093fcaa99ee28f0725e6e32f2f6637f508
tmori@esm.co.jp <tmori@esm.co.jp>
2018-10-20 13:25:05 +0900

    add multithread node

--- tools/ros/command/acomm_init.c
+++ tools/ros/command/acomm_init.c
@@ -7,26 +7,40 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <sys/file.h>
+#include <pthread.h>
 
 #include "athrill_comm.h"
 #include "athrill_comm_config.h"
 #include "athrill_comm_generated_config.h"
 
 acomm_bus_type acomm_bus[ATHRILL_COMM_CONFIG_BUS_NUM];
-static acomm_bus_metadata_type    *bus_map = NULL;
-static int fd = -1;
+static acomm_bus_metadata_type    *bus_map[ATHRILL_COMM_CONFIG_BUS_NUM];
+static int bus_fd[ATHRILL_COMM_CONFIG_BUS_NUM];
+static pthread_mutex_t acomm_mutex[ATHRILL_COMM_CONFIG_BUS_NUM];
+
+static void acomm_generated_code_init_common(acomm_uint32 busid)
+{
+    acomm_bus_metadata_type *tmp_bus_map = bus_map[busid];
+    acomm_uint8 *p = (acomm_uint8*)bus_map[busid];
+    acomm_bus[busid].num = tmp_bus_map->meta_entrynum;
+    acomm_bus[busid].meta = tmp_bus_map;
+    acomm_bus[busid].comm_buffer_offset = (acomm_uint32*)&p[tmp_bus_map->meta_buffer_offset_soff];
+    acomm_bus[busid].comm_buffer_size = (acomm_uint32*)&p[tmp_bus_map->meta_buffer_size_soff];
+    acomm_bus[busid].comm_buffer_elmsize = (acomm_uint32*)&p[tmp_bus_map->meta_buffer_elmsize_soff];
+    acomm_bus[busid].comm_buffer_earraysize = (acomm_uint32*)&p[tmp_bus_map->meta_buffer_earraysize_soff];
+    acomm_bus[busid].comm_buffer_type = (acomm_uint32*)&p[tmp_bus_map->meta_buffer_type_soff];
+    acomm_bus[busid].comm_buffer = (acomm_uint8*)&p[tmp_bus_map->data_data_soff];
+
+    return;
+}
 
 void acomm_generated_code_init(void)
 {
-    acomm_uint8 *p = (acomm_uint8*)bus_map;
-    acomm_bus[bus_map->meta_busid].num = bus_map->meta_entrynum;
-    acomm_bus[bus_map->meta_busid].meta = bus_map;
-    acomm_bus[bus_map->meta_busid].comm_buffer_offset = (acomm_uint32*)&p[bus_map->meta_buffer_offset_soff];
-    acomm_bus[bus_map->meta_busid].comm_buffer_size = (acomm_uint32*)&p[bus_map->meta_buffer_size_soff];
-    acomm_bus[bus_map->meta_busid].comm_buffer_elmsize = (acomm_uint32*)&p[bus_map->meta_buffer_elmsize_soff];
-    acomm_bus[bus_map->meta_busid].comm_buffer_earraysize = (acomm_uint32*)&p[bus_map->meta_buffer_earraysize_soff];
-    acomm_bus[bus_map->meta_busid].comm_buffer_type = (acomm_uint32*)&p[bus_map->meta_buffer_type_soff];
-    acomm_bus[bus_map->meta_busid].comm_buffer = (acomm_uint8*)&p[bus_map->data_data_soff];
+    acomm_uint32 i;
+
+    for (i = 0; i < ATHRILL_COMM_CONFIG_BUS_NUM; i++) {
+        acomm_generated_code_init_common(i);
+    }
     return;
 }
 
@@ -34,6 +48,8 @@ acomm_bus_metadata_type *acomm_open(char *path)
 {
     int err;
     struct stat statbuf;
+    acomm_bus_metadata_type *tmp_bus_map;
+    int fd;
 
 	fd = open(path, O_RDWR);
     if (fd < 0) {
@@ -45,30 +61,47 @@ acomm_bus_metadata_type *acomm_open(char *path)
         fprintf(stderr, "Error: can not stat file %s\n", path);
         return NULL;
     }
-    bus_map = (acomm_bus_metadata_type*)mmap(NULL, statbuf.st_size, (PROT_READ|PROT_WRITE), MAP_SHARED, fd, 0);
-    if (bus_map == NULL) {
+    tmp_bus_map = (acomm_bus_metadata_type*)mmap(NULL, statbuf.st_size, (PROT_READ|PROT_WRITE), MAP_SHARED, fd, 0);
+    if (tmp_bus_map == NULL) {
         fprintf(stderr, "Error: can not mmap file %s\n", path);
         return NULL;
     }
+    bus_fd[tmp_bus_map->meta_busid] = fd;
+    bus_map[tmp_bus_map->meta_busid] = tmp_bus_map;
+	pthread_mutex_init(&acomm_mutex[tmp_bus_map->meta_busid], NULL);
 
-    (void)athrill_comm_init();
+    acomm_generated_code_init_common(tmp_bus_map->meta_busid);
 
-    return bus_map;
+    return tmp_bus_map;
 }
 
 void acomm_close(acomm_bus_metadata_type *p)
 {
+    int fd;
+    if (p == NULL) {
+        return;
+    }
+    if (bus_map[p->meta_busid] != p) {
+        return;
+    }
+    fd = bus_fd[p->meta_busid];
     if (fd >= 0) {
         close(fd);
-        fd = -1;
+        bus_fd[p->meta_busid] = -1;
     }
-    bus_map = NULL;
+    bus_map[p->meta_busid] = NULL;
     return;
 }
 
 void acomm_lock(acomm_busid busid)
 {
+    int fd;
+    if (bus_map[busid] == NULL) {
+        return;
+    }
+    fd = bus_fd[busid];
     if (fd >= 0) {
+        pthread_mutex_lock(&acomm_mutex[busid]);
         flock(fd, LOCK_EX);
     }
     return;
@@ -76,8 +109,14 @@ void acomm_lock(acomm_busid busid)
 
 void acomm_unlock(acomm_busid busid)
 {
+    int fd;
+    if (bus_map[busid] == NULL) {
+        return;
+    }
+    fd = bus_fd[busid];
     if (fd >= 0) {
         flock(fd, LOCK_UN);
+        pthread_mutex_unlock(&acomm_mutex[busid]);
     }
     return;
 }
