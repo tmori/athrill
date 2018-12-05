11d918a479d895872cb2bc3ce2d8eba2c1caf464
tmori@esm.co.jp <tmori@esm.co.jp>
2018-12-05 17:14:30 +0900

    add connect

--- sample/barmetal/step6/main.c
+++ sample/barmetal/step6/main.c
@@ -10,23 +10,38 @@ unsigned char stack_data[STACK_SIZE] __attribute__ ((section(".bss_noclr")));
 
 unsigned char mmap_data[1024] __attribute__ ((section(".mmap_section")));
 
-static int gl_variable;
-
 sys_addr athrill_device_func_call __attribute__ ((section(".athrill_device_section")));
 
 
 int main(void)
 {
-	char *a = "10";
-
-	test_print_one(a);
 	timer_init();
 
 	printf("Hello World!!\n");
 
-	gl_variable = 10;
+	sys_int32 sockfd;
+	sys_int32 err;
+	struct sys_sockaddr_in sockaddr;
+	
+	sockfd = athrill_posix_socket(ATHRILL_SYSCALL_SOCKET_DOMAIN_AF_INET, ATHRILL_SYSCALL_SOCKET_TYPE_STREAM, ATHRILL_SYSCALL_SOCKET_PROTOCOL_ZERO);
+	if (sockfd < 0) {
+		printf("socket error\n");
+	}
+	else {
+		test_print_line("OK:socket=", sockfd);
+	}
 
-	(void)athrill_posix_system(100);
+	sockaddr.sin_family = ATHRILL_SYSCALL_SOCKADDR_FAMILIY_PF_INET;
+	sockaddr.sin_addr = ATHRILL_SYSCALL_IPADDR(127, 0, 0, 1);
+	sockaddr.sin_port = 50005;
+
+	err = athrill_posix_connect(sockfd, &sockaddr, sizeof(sockaddr));
+	if (err < 0) {
+		test_print_line("connect error=", -err);
+	}
+	else {
+		printf("OK:connect\n");
+	}
 
 	while (1) {
 		;
