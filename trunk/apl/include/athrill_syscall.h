4ce92794c8bea2beb9773bf24d125a668091de66
tmori@esm.co.jp <tmori@esm.co.jp>
2018-12-05 14:29:47 +0900

    refactoring

--- trunk/apl/include/athrill_syscall.h
+++ trunk/apl/include/athrill_syscall.h
@@ -41,6 +41,9 @@ struct api_arg_shutdown {
     sys_int32 sockfd;
     sys_int32 how;
 };
+struct api_arg_system {
+    sys_uint32 id;
+};
 
 typedef enum {
     SYS_API_ID_NONE = 0,
@@ -49,6 +52,7 @@ typedef enum {
     SYS_API_ID_SEND,
     SYS_API_ID_RECV,
     SYS_API_ID_SHUTDOWN,
+    SYS_API_ID_SYSTEM,
     SYS_API_ID_NUM,
 } AthrillSyscallApiIdType;
 
@@ -63,9 +67,6 @@ typedef enum {
 #define SYS_API_ERR_INVAL   -22
 #define SYS_API_ERR_BADFD   -77
 
-#ifndef ATHRILL_SYSCALL_DEVICE
-extern sys_addr athrill_device_func_call __attribute__ ((section(".athrill_device_section")));
-
 typedef struct {
     sys_uint32 api_id;
     sys_int32 ret_value;
@@ -75,13 +76,104 @@ typedef struct {
         struct api_arg_send api_send;
         struct api_arg_recv api_recv;
         struct api_arg_shutdown api_shutdown;
+        struct api_arg_system api_system;
     } body;
 } AthrillSyscallArgType;
+
+#ifndef ATHRILL_SYSCALL_DEVICE
+extern sys_addr athrill_device_func_call __attribute__ ((section(".athrill_device_section")));
 
 #define ATHRILL_SYSCALL(api_argp)   \
 do {    \
     athrill_device_func_call = (sys_addr)(api_argp);    \
 } while (0) \
+
+static inline sys_int32 athrill_posix_socket(sys_int32 domain, sys_int32 type, sys_int32 protocol)
+{
+    AthrillSyscallArgType args;
+
+    args.api_id = SYS_API_ID_SOCKET;
+    args.ret_value = SYS_API_ERR_INVAL;
+    args.body.api_socket.domain = domain;
+    args.body.api_socket.type = type;
+    args.body.api_socket.protocol = protocol;
+
+    ATHRILL_SYSCALL(&args);
+    
+    return args.ret_value;
+}
+static inline sys_int32 athrill_posix_connect(sys_int32 sockfd, const struct sys_sockaddr_in *addr, sys_uint32 addrlen)
+{
+    AthrillSyscallArgType args;
+
+    args.api_id = SYS_API_ID_CONNECT;
+    args.ret_value = SYS_API_ERR_INVAL;
+    args.body.api_connect.sockfd = sockfd;
+    args.body.api_connect.sockaddr = (sys_addr)addr;
+    args.body.api_connect.addrlen = addrlen;
+
+    ATHRILL_SYSCALL(&args);
+
+    return args.ret_value;
+}
+
+#define ATHRILL_POSIX_MSG_DONTWAIT 0
+static inline sys_uint32 athrill_posix_send(sys_int32 sockfd, const sys_addr buf, sys_uint32 len, sys_int32 flags)
+{
+    AthrillSyscallArgType args;
+
+    args.api_id = SYS_API_ID_SEND;
+    args.ret_value = SYS_API_ERR_INVAL;
+    args.body.api_send.sockfd = sockfd;
+    args.body.api_send.buf = buf;
+    args.body.api_send.len = len;
+    args.body.api_send.flags = flags;
+
+    ATHRILL_SYSCALL(&args);
+
+    return args.ret_value;
+}
+static inline sys_uint32 athrill_posix_recv(sys_int32 sockfd, sys_addr buf, sys_uint32 len, sys_int32 flags)
+{
+    AthrillSyscallArgType args;
+
+    args.api_id = SYS_API_ID_RECV;
+    args.ret_value = SYS_API_ERR_INVAL;
+    args.body.api_recv.sockfd = sockfd;
+    args.body.api_recv.buf = buf;
+    args.body.api_recv.len = len;
+    args.body.api_recv.flags = flags;
+
+    ATHRILL_SYSCALL(&args);
+
+    return args.ret_value;
+}
+
+#define ATHRILL_POSIX_SHUT_RDWR 0
+static inline sys_int32 athrill_posix_shutdown(sys_int32 sockfd, sys_int32 how)
+{
+    AthrillSyscallArgType args;
+
+    args.api_id = SYS_API_ID_SHUTDOWN;
+    args.ret_value = SYS_API_ERR_INVAL;
+    args.body.api_shutdown.sockfd = sockfd;
+    args.body.api_shutdown.how = how;
+
+    ATHRILL_SYSCALL(&args);
+
+    return args.ret_value;
+}
+
+static inline sys_int32 athrill_posix_system(sys_uint32 id)
+{
+    AthrillSyscallArgType args;
+    args.api_id = SYS_API_ID_SYSTEM;
+    args.ret_value = SYS_API_ERR_INVAL;
+
+    ATHRILL_SYSCALL(&args);
+
+    return args.ret_value;
+}
 
 #endif /* ATHRILL_SYSCALL_DEVICE */
 
